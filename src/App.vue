0a3b97aeccd54af56398bfbc5d0ca8f8aaaaac20
nnatthebest <biyakov.den@gmail.com>
2020-06-22 19:05:54 +0600

    Revert "рк"
    This reverts commit 282d541609929753a2b7866455c11b880d7af6d7.

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="catalog">
+        <ul class="catalog__list">
+          <li class="catalog__item" v-for="(product, index) in products" :key="index">
+            <a class="catalog__pic" href="#">
+              <img :src="product.img" :alt="product.title">
+            </a>
+
+            <h3 class="catalog__title">
+              <a href="#">
+                {{ product.title }}
+              </a>
+            </h3>
+
+            <span class="catalog__price">
+              {{ product.price }} ₽
+            </span>
+          </li>
+        </ul>
+      </section>
+</template>
+
+<script>
+import products from './data/products';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      products,
+    };
+  },
+};
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
