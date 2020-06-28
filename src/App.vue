7187828b37938eb9d57c64a08410f309cb0d6422
nnatthebest <biyakov.den@gmail.com>
2020-06-28 21:07:53 +0600

    Pagination

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <section class="catalog">
     <ProductList :products="products"/>
+
+    <BaseProducts v-model="page" :count="countProducts" :per-page="productPerPage" />
   </section>
 </template>
 
 <script>
 import products from './data/products';
 import ProductList from './components/ProductList.vue';
+import BaseProducts from './components/BasePagination.vue';
 
 export default {
   name: 'App',
   components: {
     ProductList,
+    BaseProducts,
   },
   data() {
     return {
-      products,
+      productPerPage: 3,
+      page: 1,
     };
+  },
+  computed: {
+    products() {
+      const offset = (this.page - 1) * this.productPerPage;
+      return products.slice(offset, offset + this.productPerPage);
+    },
+    countProducts() {
+      return products.length;
+    },
   },
 };
 </script>
